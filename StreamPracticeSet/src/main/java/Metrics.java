9e0eeda253ab8a6017276d3332dab54995a4538f
dshparko <951008d@mail.ru>
2025-09-01 18:19:17 +0300

    feat: add main class for metrics

---
+++ StreamPracticeSet/src/main/java/Metrics.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Metrics {
+    private List<Order> orders;
+
+    public Metrics(List<Order> orders) {
+        this.orders = new ArrayList<>(orders);
+    }
+
+}
