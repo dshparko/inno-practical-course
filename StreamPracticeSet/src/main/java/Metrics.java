e7cd157c8badd45e770c3d9682470b9d55b3411a
dshparko <951008d@mail.ru>
2025-09-01 18:29:21 +0300

    feat: implement method, which returns list of unique cities where orders came from

--- StreamPracticeSet/src/main/java/Metrics.java
+++ StreamPracticeSet/src/main/java/Metrics.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Metrics {
     private List<Order> orders;
 
     public Metrics(List<Order> orders) {
         this.orders = new ArrayList<>(orders);
+    }
+
+    public List<String> getUniqueCities() {
+        return orders.stream()
+                .map(Order::getCustomer)
+                .filter(Objects::nonNull)
+                .map(Customer::getCity)
+                .distinct()
+                .toList();
     }
 
 }
