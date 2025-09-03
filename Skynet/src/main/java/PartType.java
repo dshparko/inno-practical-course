0bcaf3510402810bf27ab3401535dec7ea6c011d
dshparko <951008d@mail.ru>
2025-09-03 11:47:50 +0300

    refactor: improve thread safety

--- Skynet/src/main/java/PartType.java
+++ Skynet/src/main/java/PartType.java
@@ -1,3 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public enum PartType {
-    HEAD, TORSO, HAND, FEET
+    HEAD, TORSO, HAND, FEET;
+
+    private static final PartType[] VALUES = values();
+
+    public static PartType getRandomPart() {
+        return VALUES[ThreadLocalRandom.current().nextInt(VALUES.length)];
+    }
 }
